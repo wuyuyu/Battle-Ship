0d34dd60edb17031c7265d176568e87308a916cb
wu <wu@MacBook-Pro-de-wu.local>
2019-10-28 16:52:32 +0100

    contenue

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,21 @@ public class Main {
         System.out.println("Welcome to the battle ship");
 
         String line = " ";
-        String column = " ";
-
+        // String clumn = " ";
+        System.out.println("  A B C D E F G H I J");
         char tab[][] = new char[10][10];
-        for (int i = 0; i < 10; i = i + 1) {
-            line = line + tab[0][i];
+        for (int i = 0; i < 10; i = i + 1) {        // row
+
+            line = "";   //line + tab[0][i];
             for (int j = 0; j < 10; j = j + 1) {
-                column = column + tab[i][j];
-                System.out.print(tab[i][j] + " ");
+                //column = column + tab[i][j];
+
+                line = line + tab[i][j] + " ";
+                //System.out.print(tab[i][j] + " ");
             }
-            System.out.println();
+            System.out.print(i +" ");
+            System.out.println(line);
+
         }
     }
 }
