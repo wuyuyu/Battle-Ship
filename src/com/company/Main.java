251e87f2b26647b631f5ba8e9327c1a8b8ae2e66
laurent <laurent.bodin58@gmail.com>
2019-10-28 14:54:14 +0100

    essaie1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //"coucou"
-        //hbhvgh
+        System.out.println("Welcome to the battle ship");
     }
 }
