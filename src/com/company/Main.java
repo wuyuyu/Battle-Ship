49cbeb15d9088503182d5445020b7d677b123f7f
wu <wu@MacBook-Pro-de-wu.local>
2019-10-28 16:10:26 +0100

    tableau vide

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the battle ship");
 
+        String line = " ";
+        String column = " ";
+
+        char tab[][] = new char[10][10];
+        for (int i = 0; i < 10; i = i + 1) {
+            line = line + tab[0][i];
+            for (int j = 0; j < 10; j = j + 1) {
+                column = column + tab[i][j];
+                System.out.print(tab[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
