34eed543f2a0ff44817e2f1043fbd531d8882f7c
laurent <laurent.bodin58@gmail.com>
2019-10-29 16:46:53 +0100

    createboat busy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,13 +16,26 @@ public class Main {
     public static void displayBoard(char board[][]) {
 
         String line = " ";
-        System.out.println("   A B C D E F G H I J"); // column index
+        char letter = 'A';
+
+
+        // display letters according to the array width
+        System.out.print("    "); // column index
+        for(int j=0;j<board[0].length;j=j+1){
+            System.out.print( (char)(letter+j) );
+            System.out.print(" ");
+        }
+        System.out.print("\n");
+
         for (int i = 0; i < board.length; i = i + 1) { // rows
             line = "";
-            for (int j = 0; j < board.length; j = j + 1) { // columns
+            for (int j = 0; j < board[0].length; j = j + 1) { // columns
                 line = line + board[i][j] + " ";
             }
-            System.out.print(i +"  "); // row index
+            if(i<9){
+                System.out.print(" ");
+            }
+            System.out.print( (i+1) +"  "); // row index
             System.out.println(line);
         }
     }
@@ -33,10 +46,60 @@ public class Main {
     static void initBoard (char board[][]) {
 
         for (int i = 0; i < board.length; i = i + 1) { // rows
-            for (int j = 0; j < board.length; j = j + 1) { // columns
+            for (int j = 0; j < board[0].length; j = j + 1) { // columns
                 board[i][j] = '~';
             }
         }
+        createBoat(5,board);
+        createBoat(2,board);
+        createBoat(3,board);
+        createBoat(3,board);
+        createBoat(4,board);
+
+    }
+
+    // returns a    random value between 0 included and 'maxValue' included
+    static int getRandom(int maxValue){
+        return (int)( Math.random()*(maxValue+1) );
+    }
+
+    static void createBoat (int N,char board[][]){
+        // une valeur aleatoire pour l'orientation (H / V)
+        // idxCol generer une valeur aleatoire pour la position colonne   idxCol = getRandom(9);
+        // idxRow une aleatoire pour la ligne
+
+        // on boucle sur la taille du bateau (variable i de 0 a N-1 inclu)
+        // on va ecrire le caractere # pour chaque case du bateau ( board[idxRow][idxCol+i] = '#'; ) // Horizontal
+        // on va ecrire le caractere # pour chaque case du bateau ( board[idxRow+i][idxCol] = '#'; ) // Vertical
+
+
+// encadrer le code par un while(shipExisting == true)
+
+        int HV = getRandom(1);
+        int idxRow = getRandom(9);
+        int idxCol = getRandom(9);
+        if (HV==0) {
+            idxCol = getRandom(board[0].length-N);
+        }
+        else{
+            idxRow = getRandom(board.length-N);
+        }
+
+        // boucle sur les cases du bateau 
+        // si deja une case # alors on met un booleen "shipExisting" a true
+
+
+        // encadrer ce code par if(shipExisting == false)
+        int i;
+        for(i=0;i<=N-1;i=i+1){
+            if(HV==0){
+                board[idxRow][idxCol+i] = '#';
+            }
+            else{
+                board[idxRow+i][idxCol] = '#';
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -46,7 +109,11 @@ public class Main {
 
         char playerBoard [][] = new char[10][10] ;
 
+
+
         initBoard(playerBoard);
         displayBoard(playerBoard);
+
+
     }
 }
