1a528f2751ebad25624dd98cbb0a17efef7ac06c
Ivann <ivann.galic@edu.itescia.fr>
2019-10-29 13:04:27 +0100

    Plateau de jeu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,56 @@
 package com.company;
+import java.lang.Math;
+
+/*
+ * Membres du groupe : Ivann, Laurent, Yuyuan.
+ *
+ * Description   : TP "BATAILLE NAVALE"
+ */
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to the battle ship");
+    /**
+     * initBoard() : Function which displays an empty battlefield (10 rows / 10 columns).
+     */
+    public static void initBoard(char board[][]) {
 
         String line = " ";
-        // String clumn = " ";
-        System.out.println("  A B C D E F G H I J");
-        char tab[][] = new char[10][10];
-        for (int i = 0; i < 10; i = i + 1) {        // row
-
-            line = "";   //line + tab[0][i];
-            for (int j = 0; j < 10; j = j + 1) {
-                //column = column + tab[i][j];
-
-                line = line + tab[i][j] + " ";
-                //System.out.print(tab[i][j] + " ");
+        System.out.println("   A B C D E F G H I J"); // column index
+        for (int i = 0; i < board.length; i = i + 1) { // rows
+            line = "";
+            for (int j = 0; j < board.length; j = j + 1) { // columns
+                line = line + board[i][j] + " ";
             }
-            System.out.print(i +" ");
+            System.out.print(i +"  "); // row index
             System.out.println(line);
-
         }
+    }
+
+    /**
+     * displayBoard() : Function which displays the elements of the battlefield.
+     */
+    static void displayBoard (char board[][]) {
+
+        for (int i = 0; i < board.length; i = i + 1) { // rows
+            for (int j = 0; j < board.length; j = j + 1) { // columns
+                board[i][j] = '~';
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("**** Welcome to the battleship game ! **** ");
+        System.out.println(" ");
+
+        char playerBoard [][] = new char[10][10] ;
+        char cpuBoard [][] = new char[10][10] ;
+
+        displayBoard(playerBoard);
+        initBoard(playerBoard);
+        System.out.println(" ");
+        displayBoard(cpuBoard);
+        initBoard(playerBoard);
+
     }
 }
